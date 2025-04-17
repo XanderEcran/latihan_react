265f601d11319d41ef6bb68a27e51e7ea902043b
XanderEcran <xander.cooperation@gmail.com>
2025-04-17 20:59:07 +0700

    Latihan 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const nama = "Desyana";
+const role = "superAdmin";
+
 function App() {
 
   return (
    <div>
-    <h1>Hello React</h1>
-    <p>I&apos;m boarding now</p>
+    <h1>Selamat datang {nama}</h1>
+    <p>Peran: {role} </p>
    </div>
   );
 }
